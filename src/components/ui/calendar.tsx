7e26e9d2e67f7bf8c90d260753490e250db0cc6f
Chandra Tara Raj <chandratararaj@gmail.com>
2026-02-03 21:49:13 +0530

    Fix calendar component for react-day-picker compatibility

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,8 +54,6 @@ function Calendar({
                 ...classNames,
             }}
             components={{
-                IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-                IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
             }}
             {...props}
         />
